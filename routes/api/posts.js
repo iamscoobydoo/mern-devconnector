236831146e5d2bd0cc07d202254701995c5215bc
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-03-28 21:27:36 +0530

    post and delete a comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -120,7 +120,7 @@ router.put("/like/:post_id", auth, async function (req, res) {
 });
 
 //@route    PUT api/posts/unlike/:post_id
-//@desc     Like a post
+//@desc     Unlike a post
 //@access   Private
 router.put("/unlike/:post_id", auth, async function (req, res) {
     try {
@@ -145,6 +145,81 @@ router.put("/unlike/:post_id", auth, async function (req, res) {
         return res.json(post.likes);
     } catch (err) {
         console.error(err.message);
+        return res.status(500).send("Internal Server error");
+    }
+});
+
+//@route    POST api/posts/comment/:post_id
+//@desc     Comment on a post
+//@access   Private
+router.post(
+    "/comment/:post_id",
+    [auth, [check("text", "Text is required").not().isEmpty()]],
+    async function (req, res) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const user = await User.findById(req.user.id).select("-password");
+            const post = await Post.findById(req.params.post_id);
+
+            const newComment = {
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id,
+            };
+
+            post.comments.unshift(newComment);
+            post.save();
+            return res.json(post.comments);
+        } catch (err) {
+            console.error(err.message);
+            return res.status(500).send("Internal Server error");
+        }
+    }
+);
+
+//@route    DELETE api/posts/comment/:post_id/:comment_id
+//@desc     delete a comment on a post
+//@access   Private
+router.delete("/comment/:post_id/:comment_id", auth, async function (req, res) {
+    try {
+        const post = await Post.findById(req.params.post_id);
+        if (!post) {
+            return res.status(404).json({ msg: "Post does not exist" });
+        }
+
+        //retrieve the comment from the post
+        const comment = post.comments.find(
+            (comment) => comment.id === req.params.comment_id
+        );
+
+        //check if comment exists
+        if (!comment)
+            return res.status(404).json({ msg: "Comment does not exist" });
+
+        //check if comment was made by logged in user
+        if (comment.user.toString() !== req.user.id)
+            return res.status(401).json({ msg: "User not authorised" });
+
+        //get remove index
+        const removeIndex = post.comments
+            .map((comment) => comment.user.toString())
+            .indexOf(req.params.comment_id);
+
+        post.comments.splice(removeIndex, 1);
+
+        await post.save();
+        return res.json(post.comments);
+    } catch (err) {
+        console.error(err.message);
+
+        if (err.kind == "ObjectId")
+            return res.status(400).json({ msg: "Post not found" });
+
         return res.status(500).send("Internal Server error");
     }
 });
