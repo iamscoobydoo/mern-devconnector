12fe9b1af646e41948ecf8e5076d0d5ff9023084
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-03-28 22:39:12 +0530

    create navbar and landing component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import React from "react";
+import Navbar from "./Components/Layout/Navbar";
+import Landing from "./Components/Layout/Landing";
 import "./App.css";
 
 const App = () => {
     return (
         <>
-            <h1>App</h1>
+            <Navbar />
+            <Landing />
         </>
     );
 };
