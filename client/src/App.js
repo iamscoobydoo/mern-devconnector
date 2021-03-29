fe48331b9fab1a7917cca906c517b98cfade051f
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-03-29 10:26:16 +0530

    create login and register.js and connect by router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./Components/Layout/Navbar";
 import Landing from "./Components/Layout/Landing";
+import Login from "./Components/Auth/Login";
+import Register from "./Components/Auth/Register";
 import "./App.css";
 
 const App = () => {
     return (
-        <>
-            <Navbar />
-            <Landing />
-        </>
+        <Router>
+            <>
+                <Navbar />
+                <Route exact path='/' component={Landing} />
+                <section className='container'>
+                    <Switch>
+                        <Route exact path='/register' component={Register} />
+                        <Route exact path='/login' component={Login} />
+                    </Switch>
+                </section>
+            </>
+        </Router>
     );
 };
 
