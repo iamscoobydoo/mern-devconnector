cdcfa3ed700b75456481f4409f0a4caff1205e36
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-03-29 15:34:00 +0530

    init redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 import Navbar from "./Components/Layout/Navbar";
 import Landing from "./Components/Layout/Landing";
 import Login from "./Components/Auth/Login";
@@ -8,18 +10,24 @@ import "./App.css";
 
 const App = () => {
     return (
-        <Router>
-            <>
-                <Navbar />
-                <Route exact path='/' component={Landing} />
-                <section className='container'>
-                    <Switch>
-                        <Route exact path='/register' component={Register} />
-                        <Route exact path='/login' component={Login} />
-                    </Switch>
-                </section>
-            </>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <>
+                    <Navbar />
+                    <Route exact path='/' component={Landing} />
+                    <section className='container'>
+                        <Switch>
+                            <Route
+                                exact
+                                path='/register'
+                                component={Register}
+                            />
+                            <Route exact path='/login' component={Login} />
+                        </Switch>
+                    </section>
+                </>
+            </Router>
+        </Provider>
     );
 };
 
