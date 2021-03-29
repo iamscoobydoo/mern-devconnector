72f4af4b5f7639f4ae7d4cf88bba2468a260beba
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-03-29 11:31:25 +0530

    create login and register form page

--- client/src/Components/Auth/Login.js
+++ client/src/Components/Auth/Login.js
@@ -1,10 +1,69 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+    const [formData, setFormData] = React.useState({
+        email: "",
+        password: "",
+    });
+
+    const { email, password } = formData;
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    };
+
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <>
+            <div className='alert alert-danger'>Invalid credentials</div>
+            <h1 className='large text-primary'>Sign In</h1>
+            <p className='lead'>
+                <i className='fas fa-user'></i> Sign into Your Account
+            </p>
+            <form
+                className='form'
+                action='dashboard.html'
+                onSubmit={handleSubmit}
+            >
+                <div className='form-group'>
+                    <input
+                        type='email'
+                        placeholder='Email Address'
+                        name='email'
+                        value={email}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className='form-group'>
+                    <input
+                        type='password'
+                        placeholder='Password'
+                        name='password'
+                        value={password}
+                        onChange={handleChange}
+                        autoComplete='off'
+                        required
+                    />
+                </div>
+                <input
+                    type='submit'
+                    className='btn btn-primary'
+                    value='Login'
+                />
+            </form>
+            <p className='my-1'>
+                Don't have an account? <Link to='/register'>Sign Up</Link>
+            </p>
+        </>
     );
 };
 
