29c70a62ae84241157db2b04f3a9c1c3628c2c47
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-04-02 12:57:43 +0530

    create a loading component

---
+++ client/src/Components/Layout/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+    return (
+        <>
+            <i style={{ margin: "auto" }} className='fa fa-spinner fa-spin fa-3x'></i>
+        </>
+    );
+};
+
+export default Loading;
