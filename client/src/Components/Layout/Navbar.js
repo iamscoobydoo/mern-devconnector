46f374eabe246af1420732c60fe62bcb7e1b4b04
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-04-02 12:16:47 +0530

    add dashboard link to navbar

--- client/src/Components/Layout/Navbar.js
+++ client/src/Components/Layout/Navbar.js
@@ -8,7 +8,12 @@ const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
     const authLinks = (
         <ul>
             <li>
-                <a href='#' onClick={logout}>
+                <Link to='/dashboard'>
+                    <i className='fas fa-user'></i> <span className='hide-sm'>Dashboard</span>
+                </Link>
+            </li>
+            <li>
+                <a href='#!' onClick={logout}>
                     <i className='fas fa-sign-out-alt'></i> <span className='hide-sm'>Logout</span>
                 </a>
             </li>
