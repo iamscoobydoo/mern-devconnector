e5722f07c67eb143d13fd8542a5b272dd277ee0d
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-04-02 13:05:15 +0530

    setup basic dashboard

--- client/src/Components/dashboard/Dashboard.js
+++ client/src/Components/dashboard/Dashboard.js
@@ -1,14 +1,36 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import Loading from "../layout/Loading";
 import { getCurrentProfile } from "../../actions/profile";
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
     React.useEffect(() => {
         getCurrentProfile();
-    }, []);
+    }, [getCurrentProfile]);
 
-    return <div>Dashboard</div>;
+    return loading && profile === null ? (
+        <Loading />
+    ) : (
+        <>
+            <h1 className='large text-primary'>Dashboard</h1>
+            <p className='lead'>
+                <i className='fas fa-user'></i> Welcome {user && user.name}
+            </p>
+            {profile !== null ? (
+                <>has profile</>
+            ) : (
+                <>
+                    <p>You have not yet set up a profile. Please add your info</p>
+                    <Link to='/create-profile' className='btn btn-primary my-1'>
+                        {" "}
+                        Setup Profile
+                    </Link>
+                </>
+            )}
+        </>
+    );
 };
 
 Dashboard.propTypes = {
