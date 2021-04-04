8221730ddb36d3e927ea16a2861d8400850c1baa
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-04-04 16:24:46 +0530

    display profile

---
+++ client/src/Components/profile/Profile.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import ProfileTop from "./ProfileTop";
+import ProfileAbout from "./ProfileAbout";
+import ProfileExperience from "./ProfileExperience";
+import ProfileEducation from "./ProfileEducation";
+import Loading from "../layout/Loading";
+import { getProfileById } from "../../actions/profile";
+
+const Profile = ({ match, getProfileById, profile: { profile, loading }, auth }) => {
+    React.useEffect(() => {
+        getProfileById(match.params.id);
+    }, [getProfileById, match.params.id]);
+
+    return (
+        <>
+            {profile === null || loading ? (
+                <Loading />
+            ) : (
+                <>
+                    <Link to='/profiles' className='btn btn-light'>
+                        Back To Profiles
+                    </Link>
+                    {auth.isAuthenticated &&
+                        auth.loading === false &&
+                        auth.user._id === profile.user._id && (
+                            <Link to='/edit-profile' className='btn btn-dark'>
+                                Edit your profile
+                            </Link>
+                        )}
+                    <div className='profile-grid my-1'>
+                        <ProfileTop profile={profile} />
+                        <ProfileAbout profile={profile} />
+
+                        <ProfileExperience experience={profile.experience} />
+                        <ProfileEducation education={profile.education} />
+                    </div>
+                </>
+            )}
+        </>
+    );
+};
+
+Profile.propTypes = {
+    getProfileById: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+    profile: state.profile,
+    auth: state.auth,
+});
+
+export default connect(mapStateToProps, { getProfileById })(Profile);
